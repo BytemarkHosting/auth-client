0c7acf539a3d7d55dc5a9f05f81f0ab0dae3b0cf
James F. Carter <james.carter@bytemark.co.uk>
2017-09-22 10:40:02 +0100

    added a CreateImpersonatedSession mode the the CLI

--- cli/auth-cli.go
+++ cli/auth-cli.go
@@ -12,9 +12,9 @@ import (
 
 var (
 	endpoint = flag.String("endpoint", "https://auth.bytemark.co.uk", "URL for an auth server")
-	mode     = flag.String("mode", "", "What to do. Options: ReadSession, CreateSession")
-	token    = flag.String("token", "", "Token to use. Only needed for ReadSession")
-	username = flag.String("username", "", "Username. Only needed for CreateSession")
+	mode     = flag.String("mode", "", "What to do. Options: ReadSession, CreateSession, CreateImpersonated Session")
+	token    = flag.String("token", "", "Token to use. Needed for ReadSession and CreateImpersonatedSession")
+	username = flag.String("username", "", "Username. Needed for CreateSession and CreateImpersonatedSession")
 	password = flag.String("password", "", "Password. Only needed for CreateSession")
 	yubikey  = flag.String("yubikey", "", "Yubikey OTP. Only needed for CreateSession")
 )
@@ -34,13 +34,17 @@ func main() {
 		session, err := auth.ReadSession(ctx, *token)
 		fmt.Printf("Session: %+v, err: %+v\n", session, err)
 	case "CreateSession":
-		creds := make(client.Credentials)
-		creds["username"] = *username
-		creds["password"] = *password
+		creds := client.Credentials{
+			"username": *username,
+			"password": *password,
+		}
 		if *yubikey != "" {
 			creds["yubikey"] = *yubikey
 		}
 		session, err := auth.CreateSession(ctx, creds)
+		fmt.Printf("Session: %+v, err: %+v\n", session, err)
+	case "CreateImpersonatedSession":
+		session, err := auth.CreateImpersonatedSession(ctx, *token, *username)
 		fmt.Printf("Session: %+v, err: %+v\n", session, err)
 	default:
 		fmt.Printf("Unrecognised mode: %s\n", *mode)
